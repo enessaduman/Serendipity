346bf6d52008a2d7755116116e8901543456ae8c
Büşra Ceylan <110044735+busracode@users.noreply.github.com>
2025-10-18 17:44:26 +0300

    ens

--- StudentOrginizer.py
+++ StudentOrginizer.py
@@ -6,25 +6,22 @@ temp_student=0
 def data_loader():
     with open ("student_login_info.txt", "r") as file:
         for line in file:
-            line_lst=line.strip("\n").split(",")
+            line_lst=line.strip("\n").split("-")
             student_no_password[line_lst[0]]=line_lst[1]
-choice=0
+data_loader()
 while True:
-    if choice==1:
+    student_no = input("Enter your student number: ")
+    if student_no in student_no_password:
         while True:
-            student_no = int(input("Enter your student number: "))
-            for student_nos, student_password in student_no_password:
-                if student_nos == student_no:
-                    while True:
-                        password = input("Enter your password: ")
-                        if password == student_password:
-                            temp_student = student_nos
-                            break
-                        else:
-                            print("Wrong password")
-                    break
-                else:
-                    print(f"There is no student number as {student_no}")
+            password = input("Enter your password: ")
+            if password == student_no_password[student_no]:
+                temp_student = student_no
+                break
+            else:
+                print("Wrong password")
+        break
+    else:
+        print(f"There is no student number as {student_no}")
 
 #Zeynep
 
